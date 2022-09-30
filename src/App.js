39d95d23720ccfb903f41692f7ad2c7802df0118
khma <khma@itri.org.tw>
2022-09-30 17:00:58 +0800

    Auth Test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 //import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import TableauEmbed from './components/TableauEmbed'
+
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
+      
       <h1>Workout Data</h1>
       <TableauEmbed />
-
+      
     </div>
-  );  
+  );
 }
 
-export default App;
+//export default App;
+export default withAuthenticator(App);
+//<AmplifySignOut />
