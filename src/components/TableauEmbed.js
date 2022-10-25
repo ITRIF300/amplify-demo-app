5f2f6636a25f131ad9302a762c64b5c8e1fca37e
khma <khma@itri.org.tw>
2022-10-25 15:51:26 +0800

    Update tableau 1025

--- src/components/TableauEmbed.js
+++ src/components/TableauEmbed.js
@@ -4,7 +4,7 @@ const {tableau} = window;
 
 function TableauEmbed(){
     const ref = useRef(null);
-    const url = "https://prod-apnortheast-a.online.tableau.com/t/itrif300/views/workout_summary/1_1/41f104de-4c86-418f-86d5-b25e4f2d186a/f0c03796-02df-426d-b417-5be03b6c56ef?:display_count=n&:showVizHome=n&:origin=viz_share_link";
+    const url = "https://prod-apnortheast-a.online.tableau.com/t/itrif300/views/wellshima/sheet0?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link";
     const options = {        
         width: "1600px",
         height: "800px",
